24f267963695b7813c1fcf5aad71bee411d1a32d
mgirma <51236729+mgirma@users.noreply.github.com>
2021-04-14 18:28:58 -0400

    Worked on target project

---
+++ src/components/hero.js
@@ -0,0 +1,10 @@
+import "./hero.css";
+//Mekdes
+export default function Hero() {
+  return (
+    <div className="hero">
+      <h1></h1>
+      <h2></h2>
+    </div>
+  );
+}
---
+++ src/components/tiles.js
@@ -0,0 +1,6 @@
+//kiarra
+import "./tiles.css";
+
+export default function  Tiles() {
+  return <div className="tiles">test</div>;
+}
