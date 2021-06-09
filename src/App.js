c950e1794cd4aa990f5d37347709d5f431d456d1
Edgar Muentes <EdgarJMuentes@Gmail.com>
2021-06-09 17:07:25 -0400

    minor fixes minor changes to fix Netlify deploys

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { useState } from "react";
 import Hero from "./components/hero";
 import Tiles from "./components/tiles";
 import "./styles.css";
-import targetPic from "../public/target.png";
+import targetPic from "./images/target.png";
 
 export default function App() {
-const showBoxState= useState(false)
-//peice of state
-const showBox= showBoxState[0]
-const setShowBox= showBoxState[1]
- //const [showBox, setShowBox] = useState(false);
+  const showBoxState = useState(false);
+  //peice of state
+  const showBox = showBoxState[0];
+  const setShowBox = showBoxState[1];
+  //const [showBox, setShowBox] = useState(false);
   return (
     <div className="App">
       <img id="tracingImage" src={targetPic} alt="cat" />
@@ -28,19 +28,23 @@ const setShowBox= showBoxState[1]
         <Hero
           showPopup={() => {
             console.log("hello");
-            setShowBox(!showBox)
-//peice of state
+            setShowBox(!showBox);
+            //peice of state
           }}
         />
         <Tiles />
-       { showBox && 
- <div className="Popup">
-<button onClick={() => {
-            console.log("hello");
-            setShowBox(false)
-          }}>x</button>
- </div>
-       }
+        {showBox && (
+          <div className="Popup">
+            <button
+              onClick={() => {
+                console.log("hello");
+                setShowBox(false);
+              }}
+            >
+              x
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
